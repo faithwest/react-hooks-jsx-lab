1188bfc9c05c17da0172b3cea18cbf65a8299c1c
faithwest <faithchepkoech425@gmail.com>
2023-10-25 19:11:45 +0300

    assignment done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (<div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        Your Name is a Web Developer from Your City
+     </h1>
+      </div>
+  );//the () is just another syle of writing jsx
 }
 
 export default Home;
